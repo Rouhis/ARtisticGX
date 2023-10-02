559cf71e71a5388df76b270a4b0ce7648677a3a9
rouhis <eeturouhiainen1@gmail.com>
2023-10-02 11:20:25 +0300

    return changed

--- app/src/main/java/com/example/artisticgx/MainActivity.kt
+++ app/src/main/java/com/example/artisticgx/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.artisticgx
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
@@ -12,9 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,11 +27,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
-import androidx.lifecycle.LiveData
 import com.example.artisticgx.ui.theme.ARtisticGXTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -171,8 +166,7 @@ private suspend fun getFrame(url: String): Bitmap =
         val imageUrl = URL(url)
         val connection = imageUrl.openConnection()
         val stream = connection.getInputStream()
-        val bitmap = BitmapFactory.decodeStream(stream)
-        return@withContext bitmap
+        return@withContext BitmapFactory.decodeStream(stream)
     }
 
 // Convert a given Bitmap to a ByteArray
