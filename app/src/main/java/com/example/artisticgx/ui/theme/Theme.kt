448f5526a37a23d309fe1a3356f0aaa8ed3f0196
Tuomas01 <tuomastheikkila@hotmail.fi>
2023-10-09 11:57:23 +0300

    Disable using theme based on wallpaper colours

--- app/src/main/java/com/example/artisticgx/ui/theme/Theme.kt
+++ app/src/main/java/com/example/artisticgx/ui/theme/Theme.kt
@@ -144,7 +144,7 @@ private val DarkColors = darkColorScheme(
 fun ARtisticGXTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
