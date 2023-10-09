48ae8c45ef206e4113c3f4574f89a751d8f3afbc
android <leogong3@gmail.com>
2023-10-09 16:11:31 +0300

    popup added

--- app/src/main/java/com/example/artisticgx/MainActivity.kt
+++ app/src/main/java/com/example/artisticgx/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.artisticgx
 
+import android.app.AlertDialog
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -23,7 +24,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,7 +34,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -155,8 +154,14 @@ fun ModelList(model: ArtisticViewModel, navController: NavController) {
                             modifier = Modifier
                                 .size(200.dp)
                                 .clickable {
-                                    navController.navigate("ARScreen/${it.name}")
-                                    Log.i("tiedot", "ARScreen/${it.name}")
+                                    if(!isNetworkAvailable(MyApp.appContext)){
+                                        showConfirmationDialog(navController.context, navController)
+
+                                    }else{
+                                        navController.navigate("ARScreen/${it.name}")
+                                        Log.i("tiedot", "ARScreen/${it.name}")
+                                    }
+
                                 }
                         )
                     }
@@ -198,6 +203,25 @@ private suspend fun getAndSaveModels(model: ArtisticViewModel, urls: List<String
             modelImage
         )
     }
+}
+fun showConfirmationDialog(context: Context,navController: NavController) {
+    val alertDialogBuilder = AlertDialog.Builder(context)
+    alertDialogBuilder.setTitle("Confirmation")
+    alertDialogBuilder.setMessage("Are you sure you want to perform this action?")
+
+    // Set a positive button and its click listener on the dialog
+    alertDialogBuilder.setPositiveButton("Yes") { dialog, which ->
+        navController.navigate("ARScreen/${null}")
+    }
+
+    // Set a negative button and its click listener on the dialog
+    alertDialogBuilder.setNegativeButton("No") { dialog, which ->
+        navController.navigate("GetModelsTest")
+    }
+
+    // Create and show the dialog
+    val alertDialog = alertDialogBuilder.create()
+    alertDialog.show()
 }
 
 fun isNetworkAvailable(context: Context?): Boolean {
--- app/src/main/java/com/example/artisticgx/MyApp.kt
+++ app/src/main/java/com/example/artisticgx/MyApp.kt
@@ -4,16 +4,14 @@ import android.app.Application
 import android.content.Context
 import android.util.Log
 
-class MyApp: Application() {
+class MyApp : Application() {
     companion object {
         lateinit var appContext: Context
-        lateinit var instance: MyApp
+            private set
     }
 
     override fun onCreate() {
         super.onCreate()
-        Log.d("QQQ", "MyApp onCreate()")
         appContext = applicationContext
-        instance = this
     }
 }
